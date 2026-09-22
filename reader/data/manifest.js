dd17ff89a66ed6ca01435f01e2326f16eb7abc94
Wanyi CHENG <awayicheng@gmail.com>
2026-09-22 15:47:03 +0800

    Add files via upload

--- reader/data/manifest.js
+++ reader/data/manifest.js
@@ -2,5 +2,9 @@ window.ISSUE_MANIFEST = [
   {
     "slug": "the-economist-uk-22-august-2026",
     "title": "The Economist UK - 22 August 2026"
+  },
+  {
+    "slug": "the-economist-uk-8-august-2026",
+    "title": "The Economist UK - 8 August 2026"
   }
 ];
